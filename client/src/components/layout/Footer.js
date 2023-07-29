ccc4bd1e815c525dd206c81a979cbf48e89023c2
Nyck Jean-Philippe <nyckjeanphilippe@gmail.com>
2023-07-29 17:51:07 -0400

    footer-content

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,11 +1,44 @@
 import React from "react";
 
 const Footer = () => {
+    const socials = [
+        {
+            name: "Gmail",
+            href: "mailto:nyckjeanphilippe@gmail.com",
+            icon: <i className="fa-regular fa-envelope" />
+        },
+        {
+            name: "LinkedIn",
+            href: "https://www.linkedin.com/in/nyckjeanphilippe/",
+            icon: <i className="fa-brands fa-linkedin" />
+        },
+        {
+            name: "GitHub",
+            href: "https://github.com/NyckJP",
+            icon: <i className="fa-brands fa-github" />
+        }
+    ]
+
+    let socialsRow = socials.map((link) => {
+        return (
+            <a key={link.name} href={link.href}>
+                {link.icon}
+            </a>
+        )
+    })
+
     //About the Developer
     //Headshot and links to linkedin and github
     return (
         <footer className="footer">
-            
+            <div className="footer-content">
+                <div className="about-me">
+                    <a href="#">Made by Nyck Jean-Philippe</a>
+                </div>
+                <div className="socials">
+                    {socialsRow}
+                </div>
+            </div>
         </footer>
     )
 }
